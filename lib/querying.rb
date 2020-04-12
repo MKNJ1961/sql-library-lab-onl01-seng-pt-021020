78d3d7a9be3036d6ace41ea4318ef688f1ef560a
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-04-12 03:38:27 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -30,14 +30,18 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT title
-   FROM series
-   INNER JOIN characters
-   ON series_id = series.id
-   WHERE species = 'human'
-   GROUP BY title
-   ORDER BY COUNT(*)
-   DESC LIMIT 1;"
+  "SELECT series.title 
+ FROM series 
+ JOIN books 
+ ON books.series_id = series.id 
+ JOIN character_books 
+ ON character_books.book_id = books.id 
+ JOIN characters 
+ ON character_books.character_id = characters.id 
+ WHERE characters.species = 'human' 
+ GROUP BY series.title 
+ ORDER BY COUNT(*) 
+ DESC LIMIT 1;"s
 end
 
 def select_character_names_and_number_of_books_they_are_in
